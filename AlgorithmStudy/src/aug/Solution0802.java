5de6eb171aa17f96e1e34a2fa3a984a494a514aa
jiho.sung <slayer1000@gmail.com>
2021-08-01 20:25:52 +0900

    8/2일 문제 처리

---
+++ AlgorithmStudy/src/aug/Solution0802.java
@@ -0,0 +1,24 @@
+package aug;
+
+import java.util.*;
+
+// https://leetcode.com/explore/featured/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3835/
+public class Solution0802 {
+
+
+    public int largestIsland(int[][] grid) {
+        int area = 0;
+
+
+        return area;
+    }
+    
+    public static void main(String[] args) {
+        Solution0802 solution0802 = new Solution0802();
+
+        // int[][] grid = {{1,1},{0,1}};
+        int[][] grid = {{1,1,0,0,1},{1,1,0,0,1},{1,1,0,1,1},{1,1,0,1,1},{1,1,0,1,1}};
+
+        System.out.println(solution0802.largestIsland(grid));
+    }
+}
