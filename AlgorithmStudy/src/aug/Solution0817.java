ab0c518ce34928100229d211cd3450ee85b743ae
jiho.sung <jiho.sung@navercorp.com>
2021-08-18 14:20:55 +0900

    8/18일 문제 풀이

--- AlgorithmStudy/src/aug/Solution0817.java
+++ AlgorithmStudy/src/aug/Solution0817.java
@@ -22,6 +22,51 @@ public class Solution0817 {
     }
 }
 
+// https://leetcode.com/problems/range-sum-query-mutable/
+// 연결 문제를 풀다가 시간 초과로 실패
+class NumArray {
+    private int[] chunkSum;
+    private int len;
+    private int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+        double chunkSize = Math.sqrt(nums.length);
+        len = (int) Math.ceil(nums.length / chunkSize);
+        chunkSum = new int[len];
+        for (int i = 0; i < nums.length; i++)
+            chunkSum[i / len] += nums[i];
+    }
+
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        int startBlock = i / len;
+        int endBlock = j / len;
+        if (startBlock == endBlock) {
+            for (int k = i; k <= j; k++)
+                sum += nums[k];
+        } else {
+            for (int k = i; k <= (startBlock + 1) * len - 1; k++)
+                sum += nums[k];
+            for (int k = startBlock + 1; k <= endBlock - 1; k++)
+                sum += chunkSum[k];
+            for (int k = endBlock * len; k <= j; k++)
+                sum += nums[k];
+        }
+        return sum;
+    }
+
+    public void update(int i, int val) {
+        int chunkIndex = i / len;
+        chunkSum[chunkIndex] = chunkSum[chunkIndex] - nums[i] + val;
+        nums[i] = val;
+    }
+}
+
+/**
+ *
+ */
+
 
 /**
  * Your NumArray object will be instantiated and called as such:
