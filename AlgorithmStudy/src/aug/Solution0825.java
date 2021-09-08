5177b498c93f390711a2a28056d3d1c257edc165
jiho.sung <jiho.sung@navercorp.com>
2021-09-08 17:35:00 +0900

    9/8일 문제 풀이

--- AlgorithmStudy/src/aug/Solution0825.java
+++ AlgorithmStudy/src/aug/Solution0825.java
@@ -19,3 +19,4 @@ public class Solution0825 {
     }
 
 }
+
---
+++ AlgorithmStudy/src/sep/Solution0908.java
@@ -0,0 +1,25 @@
+package sep;
+
+import model.ListNode;
+
+import java.util.Stack;
+
+// https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3966/
+public class Solution0908 {
+
+    public ListNode reverseList(ListNode head) {
+        // linked list 뒤집기
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null) {
+            ListNode nextNode = current.next;
+
+            current.next = prev;
+            prev = current;
+
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
