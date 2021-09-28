758389b7c8fd9fece857f378c57612c44ac17de6
jiho.sung <jiho.sung@navercorp.com>
2021-09-28 15:45:45 +0900

    9/28일 문제 풀이

--- AlgorithmStudy/src/sep/Solution0927.java
+++ AlgorithmStudy/src/sep/Solution0927.java
@@ -2,64 +2,233 @@ package sep;
 
 // https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3988/
 public class Solution0927 {
+    // Array, Math, Bit Manipulation, Matrix
+    // 실패 ㅠㅠ
     public int movesToChessboard(int[][] board) {
         int len = board.length;
-        int rowSum = 0;
-        int colSum = 0;
-        int rowSwap = 0;
-        int colSwap = 0;
+        boolean even = (len % 2) == 0;
+        int start = board[0][0];
 
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; ++j) {
-                if ((board[0][0] ^ board[i][0] ^ board[0][j] ^ board[i][j]) == 1) {
+        // 유효한가는
+        // len 가 짝수면 1, 0의 개숫가 len / 2
+        // len 가 홀수면 1, 0의 개숫가 len / 2, len / 2 + 1
+        /*
+        [1,0,1,0]
+        [0,1,0,1]
+        [1,0,1,0]
+        [0,1,0,1]
+         */
+        int[] rowCount = new int[len];
+        int[] colCount = new int[len];
+        for (int i = 0; i < len; i++) { // row
+            for (int j = 0; j < len; j++) { // col
+                rowCount[i] += board[i][j];
+                colCount[i] += board[j][i];
+                // colCount[i] += board[i][j];
+            }
+        }
+        int half = len / 2;
+        if (even) {
+            for (int i = 0; i < len; i++) {
+                if (rowCount[i] != half || colCount[i] != half) {
                     return -1;
                 }
             }
+        } else {
+            int cnt1 = half;
+            int cnt2 = len - half;
+            // 5
+            // colcount = (3, 3, 3, 2, 2) => cnt1 = 0, cnt2 = 0
+            // colcount = (2, 2, 2, 3, 3) => cnt1 = -1, cnt2 = 1
+            // cnt1 = 2, cnt2 = 3
+            for (int i = 0; i < len; i++) {
+                if (colCount[i] == half) cnt1--;
+                if (colCount[i] == (len-half)) cnt2--;
+            }
+            if (!((cnt1 == 0 && cnt2 == 0) || (cnt1 == -1 && cnt2 == 1))) {
+                return -1;
+            }
+            cnt1 = half;
+            cnt2 = len - half;
+            for (int i = 0; i < len; i++) {
+                if (rowCount[i] == half) cnt1--;
+                if (rowCount[i] == (len-half)) cnt2--;
+            }
+            if (!((cnt1 == 0 && cnt2 == 0) || (cnt1 == -1 && cnt2 == 1))) {
+                return -1;
+            }
+            if (cnt1 == 0 && cnt2 == 0) {
+                start = 1;//board[0][0] // or 1
+            } else {
+                start = 0;
+            }
         }
-        for (int i = 0; i < len; ++i) {
-            rowSum += board[0][i];
-            colSum += board[i][0];
-            if (board[i][0] == i % 2) rowSwap ++;
-            if (board[0][i] == i % 2) colSwap ++ ;
+        int swap = 0;
+        for (int i = 0; i < len; i++) {
+            if (board[i][0] != start) {
+                // swap 해야함
+                int j = i + 1;
+                boolean needSwap = false;
+                for (; j < len; j+=2) {
+                    if (board[j][0] == start) {
+                        needSwap = true;
+                        break;
+                    }
+                }
+                if (needSwap) {
+                    // i < - > j 번째 row를 변경 board[i] board[j]
+                    int[] temp = board[i];
+                    board[i] = board[j];
+                    board[j] = temp;
+                    /*for (int k = 0; k < len; k++) {
+                        int temp = borad[i][k];
+                        borad[i][k] = board[j][k];
+                        borad[j][k] = temp;
+                    }*/
+                    swap++;
+                }
+            }
+            start = (start + 1) % 2;
         }
-        if (rowSum != len / 2 && rowSum != (len + 1) / 2) return -1;
-        if (colSum != len / 2 && colSum != (len + 1) / 2) return -1;
+        start = board[0][0];
+        for (int i = 0; i < len; i++) {
+            if (board[0][i] != start) {
+                // swap 해야함
+                int j = i + 1;
+                boolean needSwap = false;
+                for (; j < len; j+=2) {
+                    if (board[0][j] == start) {
+                        needSwap = true;
+                        break;
+                    }
+                }
+                if (needSwap) {
+                    for (int k = 0; k < len; k++) {
+                        int temp = board[k][j];
+                        board[k][j] = board[k][i];
+                        board[k][i] = temp;
+                    }
+                    swap++;
+                }
+            }
+            start = (start + 1) % 2;
+        }
 
-        return 0;
+        start = board[0][0];
+        for (int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                if (board[i][j] != start) {
+                    return -1;
+                }
+                start = (start + 1) % 2;
+            }
+            if (even) {
+                start = (start + 1) % 2;
+            }
+        }
+
+        return swap;
+    }
+
+    // https://leetcode.com/problems/transform-to-chessboard/discuss/114847/C%2B%2BJavaPython-Solution-with-Explanation
+    public int movesToChessboardSolution(int[][] b) {
+        int N = b.length, rowSum = 0, colSum = 0, rowSwap = 0, colSwap = 0;
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                if ((b[0][0] ^ b[i][0] ^ b[0][j] ^ b[i][j]) == 1) return -1;
+
+        for (int i = 0; i < N; ++i) {
+            rowSum += b[0][i];
+            colSum += b[i][0];
+            if (b[i][0] == i % 2) rowSwap ++;
+            if (b[0][i] == i % 2) colSwap ++ ;
+        }
+
+        if (rowSum != N / 2 && rowSum != (N + 1) / 2) return -1;
+        if (colSum != N / 2 && colSum != (N + 1) / 2) return -1;
+
+        if (N % 2 == 1) { // 홀수판이면
+            if (colSwap % 2 == 1) colSwap = N - colSwap;
+            if (rowSwap % 2 == 1) rowSwap = N - rowSwap;
+        } else {
+            colSwap = Math.min(N - colSwap, colSwap);
+            rowSwap = Math.min(N - rowSwap, rowSwap);
+        }
+        return (colSwap + rowSwap) / 2;
     }
 
     public static void main(String[] args) {
         Solution0927 s = new Solution0927();
+        /*System.out.println(s.movesToChessboard(new int[][]{
+                {1,0,1,0},
+                {1,0,1,0},
+                {0,1,0,1},
+                {0,1,0,1}
+        }));
         System.out.println(s.movesToChessboard(new int[][]{
-                {1, 0},
-                {1, 0}
+                {0,0,0,1,1},
+                {0,0,0,1,1},
+                {0,0,0,1,1},
+                {1,1,1,0,0},
+                {1,1,1,0,0}
+        }));
+        System.out.println(s.movesToChessboard(new int[][]{ // -1
+                {1,0,0,1,1},
+                {0,1,0,1,0},
+                {0,0,0,0,1},
+                {1,1,1,0,0},
+                {1,1,1,0,0}
+        }));
+
+        System.out.println(s.movesToChessboard(new int[][]{ // 답 2
+                {1,1,0},
+                {0,0,1},
+                {0,0,1}
+        }));*/
+
+        System.out.println(s.movesToChessboard(new int[][]{ // 답 1, 우리꺼는 -1
+                {1,0,0},
+                {0,1,1},
+                {1,0,0}
         }));
     }
 
 }
 
 /*
-https://leetcode.com/problems/transform-to-chessboard/discuss/114847/C%2B%2BJavaPython-Solution-with-Explanation
-유효한 체스판은
- 010101 -> 101010 이 쌍으로 존재 해야함 (row / col) 모두
- 0011 => valid
- 1100
- 1100
- 0011
- 0011 => not valid
- 1100
- 1100
- 1010
-
- N * N 의 체스판에서
- N = 2K (짝수) 이면 K개의 1과 0이 존재
- N = 2K + 1(홀수) 이면 K개의 1, K + 1개의 0 or K + 1개의 1 or K개의 0 이 존재해야함
-
 교체는
  N이 짝수일때와 N이 홀수 일때로 나눠서 처
-
  [[1,0],
   [1,0]]
 
+[1,0,1]
+[0,1,0]
+[0,1,0]
+
+[1,0,1,0]
+[1,0,1,0]
+[1,0,1,0]
+[1,0,1,0]
+
+
+[1,0,1,0]
+[0,1,0,1]
+[1,0,1,0]
+[0,1,0,1]
+---------
+[0,1,0,1]
+[1,0,1,0]
+[0,1,0,1]
+[1,0,1,0]
+
+[0,1,0,1]
+[0,1,0,1]
+[1,0,1,0]
+[1,0,1,0]
+
+[0,0,1,1] => not valid
+[1,0,1,0]
+[0,1,0,1]
+[1,1,0,0]
 
  */
---
+++ AlgorithmStudy/src/sep/Solution0928.java
@@ -0,0 +1,77 @@
+package sep;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/639/week-4-september-22nd-september-28th/3989/
+public class Solution0928 {
+    public int numUniqueEmails(String[] emails) {
+        // 이메일 규칙
+        //  . 은 없다고 보면됨. jiho.sung == jihosung
+        //  + 는 + 뒤에는 무시함 jiho.sung+naver == jiho.sung+wev == jihosung
+        Set<String> emailSet = new HashSet<>();
+
+        for (String email : emails) {
+            String[] s = email.split("@");
+            s[0] = s[0].replaceAll("\\.", "").split("\\+")[0];
+
+            emailSet.add(s[0] + "@" + s[1]);
+        }/*
+
+        for (String email : emails) {
+            //String[] arr = email.split("@");
+            //String local = arr[0];
+            //String domain = arr[1];
+            StringBuilder builder = new StringBuilder();
+            int atIndex = -1;
+            int plusIndex = -1;
+            for (int i = 0; i < email.length(); i++) {
+                char c = email.charAt(i);
+                if (c == '+') {
+                    plusIndex = i;
+                    continue;
+                }
+
+                if (c == '@') {
+                    atIndex = i;
+                    break;
+                }
+
+                if (plusIndex < 0 && c != '.') {
+                    builder.append(c);
+                }
+            }
+
+            uniqueEmails.add(builder + "@" + email.substring(atIndex));
+        }*/
+
+        return emailSet.size();
+    }
+
+    private int computeHash(String email){
+        // DJBHash
+        int hash = 5381; // will have less collisions
+
+        // read untill @ or + which ever comes first
+        for(int i=0; i< email.length(); i++){
+            char ch = email.charAt(i);
+            if( ch == '.') continue;
+            else if(ch != '@' && ch != '+')
+                hash = (hash << 5) + hash + ch; // hash * 33 + ch
+            else
+                break;
+        }
+
+        // read untill @
+        for(int i= email.length()-1; i>=0; i--){
+            char ch = email.charAt(i);
+            if(ch != '@'){
+                hash = (hash << 5) + hash + ch; // hash * 33 + ch
+            }
+            else
+                break;
+        }
+
+        return hash;
+    }
+}
