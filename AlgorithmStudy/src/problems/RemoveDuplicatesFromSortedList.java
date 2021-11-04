08817c80599dbe373bb7602f608642dd08e9c454
jiho.sung <jiho.sung@navercorp.com>
2021-11-04 14:49:51 +0900

    11월 문제 풀이

---
+++ AlgorithmStudy/src/problems/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,20 @@
+package problems;
+
+import model.ListNode;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+
+
+        ListNode node = head;
+        while(node != null) {
+            while (node.next != null && node.val == node.next.val) {
+                node.next = node.next.next;
+            }
+            node = node.next;
+        }
+
+        return head;
+    }
+}
