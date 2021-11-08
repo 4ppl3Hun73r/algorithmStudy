2947d50ab9c0ae2e422c4b3ac69284cd32b241fd
jiho.sung <jiho.sung@navercorp.com>
2021-11-08 17:46:20 +0900

    11월 문제 풀이

---
+++ AlgorithmStudy/src/problems/PathSum.java
@@ -0,0 +1,17 @@
+package problems;
+
+import model.TreeNode;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
