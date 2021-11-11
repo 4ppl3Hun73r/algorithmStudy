a29d5072c6de2159fd5177d2b34529016a36317c
jiho.sung <jiho.sung@navercorp.com>
2021-11-11 17:16:52 +0900

    11월 문제 풀이

---
+++ AlgorithmStudy/src/problems/ValidateBinarySearchTree.java
@@ -0,0 +1,21 @@
+package problems;
+
+import model.TreeNode;
+
+// https://leetcode.com/problems/validate-binary-search-tree/
+public class ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode node, Integer min, Integer max) {
+        if (node == null) {
+            return true;
+        }
+        if ((min != null && node.val <= min) || (max != null && node.val >= max)) {
+            return false;
+        }
+
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+}
