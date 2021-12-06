2e17f6e3649349d9538045365183d2ed2390d8e8
jiho.sung <jiho.sung@navercorp.com>
2021-12-06 17:06:23 +0900

    12월 문제 풀이

---
+++ AlgorithmStudy/src/dec/Solution1206.java
@@ -0,0 +1,24 @@
+package dec;
+
+// https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+public class Solution1206 {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < position.length; i++) {
+            if (position[i] % 2 == 0) {
+                even++;
+            } else {
+                odd ++;
+            }
+        }
+        return Math.min(even, odd);
+
+        /*
+          0 1 2 3
+              ^ ^
+          0 1 2 3 4
+              ^ ^ ^
+         */
+    }
+}
